544255a511b98ed26d7d974e852c30332dd3a0a2
Matthias Wauer <matthiaswauer@googlemail.com>
2014-05-07 15:03:00 +0200

    Added structures definition.

---
+++ Quizduell_Structures.h
@@ -0,0 +1,44 @@
+#include <Eina.h>
+
+#define NO_ROUNDS_PER_GAME 6
+
+// append to list items
+typedef struct
+{
+    unsigned long id;
+} Qd_Game_Id;
+
+typedef enum
+{
+    QD_CATEGORY_BLA = 0
+} Qd_Category;
+
+typedef struct
+{
+    unsigned long user_id;
+    Eina_Stringshare *name;
+    unsigned int avatar;
+} Qd_Player;
+
+typedef enum
+{
+    QD_GAME_STATE_NONE = 0,
+    QD_GAME_STATE_PLAYING,
+    QD_GAME_STATE_FINISHED
+} Qd_Game_State;
+
+typedef struct
+{
+    Qd_Category cat_choices[NO_ROUNDS_PER_GAME];
+    unsigned short elapsed_min;
+    unsigned long game_id;
+    Eina_List *messages;
+    Qd_Player opponent;
+    unsigned int opponent_answers[NO_ROUNDS_PER_GAME][3];
+    unsigned int rating_bonus;
+    Qd_Game_State state;
+    unsigned int your_answers[NO_ROUNDS_PER_GAME][3];
+    Eina_Bool your_turn;
+} Qd_Game_Info;
+
+
